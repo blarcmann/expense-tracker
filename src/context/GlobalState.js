4a9aa7b9a198f5d39d54378bfd8d0702955c3c4b
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-02-23 14:18:16 +0100

    finalizing...

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from './AppReducer';
 
 
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
-  ]
+  transactions: []
 }
 
 
@@ -18,9 +13,25 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    })
+  }
+
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    })
+  }
+
   return (
     <GlobalContext.Provider value={{
-      transactions: state.transactions
+      transactions: state.transactions,
+      deleteTransaction,
+      addTransaction
     }}>
       {children}
     </GlobalContext.Provider>
